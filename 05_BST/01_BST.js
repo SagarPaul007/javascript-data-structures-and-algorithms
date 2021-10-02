e676cc05d24837f54f136335b32605cb2ef81c6c
Sagar <sagarpaul2k16@gmail.com>
2021-10-02 08:51:49 +0530

    bst insert

---
+++ 05_BST/01_BST.js
@@ -0,0 +1,48 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        let newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+
+        let current = this.root
+        while(true) {
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode
+                    return this
+                }
+                current = current.left
+            }else if(value > current.value){
+                if(current.right === null){
+                    current.right = newNode
+                    return this
+                }
+                current = current.right
+            }else return undefined
+        }
+    }
+}
+
+let tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+console.log(tree)
